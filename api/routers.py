3f2a2fd622d4e89842ff77a7ad6748c74da63a84
GandhiAndrianarivony <gandhi.andrianarivony@gmail.com>
2024-02-06 15:10:44 +0300

    Create blog user

--- api/routers.py
+++ api/routers.py
@@ -1,10 +1,14 @@
-from typing import Optional
+from typing import List
 
-from fastapi import FastAPI, Depends, status, Response
+from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
+from api import db_connection
 
-from apps.blogs.schema import Blog
-from apps.blogs import controllers as blog_controllers, db_connection
+from apps.blogs import schema as blog_schema
+from apps.users import schema as user_schema
+
+from apps.blogs import controllers as blog_controllers
+from apps.users import controllers as user_controllers
 
 
 db_connection.Base.metadata.create_all(bind=db_connection.engine)
@@ -12,30 +16,41 @@ db_connection.Base.metadata.create_all(bind=db_connection.engine)
 app = FastAPI()
 
 
-@app.get("/blog")
+@app.get("/blog", response_model=List[blog_schema.ShowBlog])
 def list(db: Session = Depends(db_connection.get_db)):
     return blog_controllers.get_blogs(db)
 
 
-@app.get("/blog/unpublished")
-def unpublished():
-    return {"data": "All unpublished blogs"}
-
-
-@app.get("/blog/{id}", status_code=status.HTTP_200_OK)
-def detail(id: int, response: Response, db: Session = Depends(db_connection.get_db)):
+@app.get(
+    "/blog/{id}/", 
+    status_code=status.HTTP_200_OK, 
+    response_model=blog_schema.ShowBlog
+)
+def detail(id: int, db: Session = Depends(db_connection.get_db)):
     blog = blog_controllers.get_blog(id, db)
     if not blog:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail": f"Blog with id {id} not found"}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
     return blog
 
 
-@app.get("/blog/{id}/comments/")
-def comments(id):
-    return {"data": ["1", "2"]}
-
-
 @app.post("/blog", status_code=status.HTTP_201_CREATED)
-def create(request: Blog, db: Session = Depends(db_connection.get_db)):
+def create(request: blog_schema.Blog, db: Session = Depends(db_connection.get_db)):
     return blog_controllers.create_blog(request, db)
+
+
+@app.delete("/blog/{id}/", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(db_connection.get_db)):
+    blog_controllers.delete_blog(id, db)
+    return {"message": "DELETED", "status_code": status.HTTP_200_OK}
+
+
+@app.put("/blog/{id}/", status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: blog_schema.Blog, db: Session = Depends(db_connection.get_db)):
+    return blog_controllers.update_blog(id, request, db)
+
+
+@app.post('/user')
+def create_user(request: user_schema.User,db: Session = Depends(db_connection.get_db)):
+    return user_controllers.create_user(request, db)
